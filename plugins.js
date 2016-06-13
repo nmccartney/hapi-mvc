47ea58c4ea48fbc7e7d3d6e696f8b8e257ef2d10
Nathan McCartney <nmcCartney@winsupers-MacBook-Pro.local>
2016-06-13 11:25:39 -0400

    added plugincs

---
+++ plugins.js
@@ -0,0 +1,67 @@
+
+var dogwaterOptions = {
+  connections: {
+    theDB : {
+      adapter: 'theDB'
+    }
+  },
+  adapters:{
+     theDB : 'sails-disk'
+  },
+  models: require('./models')
+};
+
+const options = {
+    ops: {
+        interval: 1000
+    },
+    reporters: {
+        console: [{
+              module: 'good-squeeze',
+              name: 'Squeeze',
+              args: [{ log: '*', response: '*' , error:'*'}]
+          }, 
+          {
+              module: 'good-console',
+              args: [{ops:'*', log: '*', response: '*' , error:'*'}]
+          }, 'stdout'],
+        file: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ ops: '*' }]
+        }, {
+            module: 'good-squeeze',
+            name: 'SafeJson'
+        }, {
+            module: 'good-file',
+            args: ['./test/fixtures/awesome_log']
+        }],
+        http: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ error: '*' }]
+        }, {
+            module: 'good-http',
+            args: ['http://prod.logs:3000', {
+                wreck: {
+                    headers: { 'x-api-key': 12345 }
+                }
+            }]
+        }]
+    }
+};
+
+
+module.exports = [
+	{
+    register : require('blipp')
+  },
+	{
+    register: require('dogwater'),
+    options: dogwaterOptions
+  },
+  {
+    register: require('good'),
+    options,
+  }
+]
